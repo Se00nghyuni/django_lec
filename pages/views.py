7ced22e93e8b85f1616c7adb6186d5af09ed4cce
Your Name <your_email@example.com>
2024-04-22 00:32:03 +0900

    2주차 django 개발 시작

---
+++ pages/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+def mainpage(request):
+    return render(request, 'pages/mainpage.html')
+
+def company(request):
+    return render(request, 'pages/company_info.html')
+
+def login(request):
+    return render(request, 'pages/login.html')
+# Create your views here.
